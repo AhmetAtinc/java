87f4055057509237eb84223de5327f84180af54f
AhmetAtinc <94438739+AhmetAtinc@users.noreply.github.com>
2022-02-02 12:57:02 +0300

    Q

---
+++ ForLoop/ForLoop2.java
@@ -0,0 +1,19 @@
+package day16_forLoops;
+
+public class C04_ForLoop {
+
+	public static void main(String[] args) {
+		// 100'den baslayarak 50'ye kadar olan sayilari aralarinda virgul olarak ayni satira yazdirin
+
+		
+		
+		for (int i = 100; i >50 ; i--) {
+			System.out.print(i +", ");
+		}
+		System.out.println(50);
+		
+		
+
+	}
+
+}
