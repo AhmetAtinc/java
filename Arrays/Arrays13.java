17cc275d06f1cac3ff92507df2068725ed089601
AhmetAtinc <94438739+AhmetAtinc@users.noreply.github.com>
2022-03-03 00:05:31 +0300

    Q

---
+++ Arrays/Arrays13.java
@@ -0,0 +1,25 @@
+package halukHocadanGelenler1._JavaInterviewPrep.interviewQuestions;
+
+public class DuplicateValueInArray {
+
+	public static void main(String[] args) {
+		/*5----
+		Tekrar eden sayilari yazdiriniz
+		input :  {1,2,3,4,5,6,6}
+		output : 6
+		*/
+
+		int arr[] = {1,2,3,4,5,6,6};
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					System.out.println(arr[i]);
+				}
+			}
+		}
+		
+
+	}
+
+}
